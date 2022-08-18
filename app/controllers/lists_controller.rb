204a1e618a52efc257c039be4d44cffdba52b1f7
Mohammed Karim Zinoun <zinoun.mkarim@gmail.com>
2022-08-18 16:58:09 +0200

    back end done and started the front in the index list page

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,7 +5,7 @@ class ListsController < ApplicationController
 
   def show
     @list = List.find(params[:id])
-    @movies = @list.movies
+    @bookmarks = Bookmark.where(list_id: params[:id])
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  resources :lists, only: %i[index show new create]
+  resources :lists, only: %i[index show new create] do
+    resources :bookmarks, only: %i[new create]
+  end
+
+  resources :bookmarks, only: [:destroy]
 end
