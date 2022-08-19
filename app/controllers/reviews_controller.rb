720fc780946b2c2082a68104a4911d902c7c5292
Mohammed Karim Zinoun <zinoun.mkarim@gmail.com>
2022-08-19 17:29:34 +0200

    app with review model but star rating js not working

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,23 @@
+class ReviewsController < ApplicationController
+  def new
+    @list = List.find(params[:list_id])
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @list = List.find(params[:list_id])
+    @review.list = @list
+    if @review.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'lists#index'
   resources :lists, only: %i[show new create] do
     resources :bookmarks, only: %i[new create]
+    resources :reviews, only: %i[new create]
   end
 
   resources :bookmarks, only: [:destroy]
